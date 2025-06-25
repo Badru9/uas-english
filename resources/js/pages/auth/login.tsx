2ac5a8a531adc031b837f52816c3ea011f0ceedc
Badru9 <mohbadrujaman9@gmail.com>
2025-06-25 21:31:54 +0700

    feat: update form login

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,20 +1,23 @@
 import { Button, Checkbox, Form, Input, Link, Spinner } from '@heroui/react';
 import { useForm } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
+import { z } from 'zod';
 
-type LoginForm = {
-    email: string;
-    password: string;
-    remember: boolean;
-};
+const LoginFormSchema = z.object({
+    email: z.string().email('Email is not valid'),
+    password: z.string().min(8, 'Password at least 8 character'),
+    remember: z.boolean(),
+});
+
+type LoginForm = z.infer<typeof LoginFormSchema>;
 
 interface LoginProps {
-    status?: string;
+    // status?: string;
     canResetPassword: boolean;
 }
 
-export default function Login({ status, canResetPassword }: LoginProps) {
-    const { data, setData, post, processing, errors, reset } = useForm<Required<LoginForm>>({
+export default function Login({ canResetPassword }: LoginProps) {
+    const { data, setData, post, processing, reset } = useForm<LoginForm>({
         email: '',
         password: '',
         remember: false,
@@ -28,16 +31,24 @@ export default function Login({ status, canResetPassword }: LoginProps) {
     };
 
     return (
-        <main className="bg-primary flex min-h-screen w-full items-center justify-center">
-            <img src="/shape-bg.jpg" alt="shape-bg" className="fixed inset-0" />
+        <main className="relative flex min-h-screen w-full items-center justify-center">
+            <img src="/shape-bg.png" alt="shape-bg" className="fixed inset-0 -z-10" />
 
-            <Form className="flex flex-col gap-6 bg-white" onSubmit={submit}>
-                <div className="grid gap-6">
+            <Form className="bg-smoke-white flex flex-col gap-6 rounded-3xl p-10" onSubmit={submit}>
+                <div className="flex flex-col items-center justify-center space-y-2 text-slate-800">
+                    <h3 className="text-2xl font-semibold">Login to Account</h3>
+                    <p className="font-semibold text-slate-600">Please enter your email and password to continue</p>
+                </div>
+                <div className="flex w-full flex-col gap-3">
                     <Input
                         id="email"
                         label="Email"
                         labelPlacement="outside"
+                        aria-label="input email"
+                        placeholder="fahmi121@gmail.com"
                         type="email"
+                        color="primary"
+                        radius="sm"
                         required
                         autoFocus
                         tabIndex={1}
@@ -57,7 +68,11 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                         id="password"
                         label="Password"
                         labelPlacement="outside"
+                        aria-label="input password"
+                        color="primary"
+                        radius="sm"
                         type="password"
+                        placeholder="*****"
                         required
                         tabIndex={2}
                         autoComplete="current-password"
@@ -65,18 +80,20 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                         onChange={(e) => setData('password', e.target.value)}
                     />
 
-                    <div className="flex items-center space-x-3">
+                    <div className="flex items-center text-slate-800">
                         <Checkbox
                             id="remember"
                             name="remember"
+                            aria-label="remember password"
                             checked={data.remember}
                             onClick={() => setData('remember', !data.remember)}
                             tabIndex={3}
-                            placeholder="Remember Me"
-                        />
+                        >
+                            Remember Password
+                        </Checkbox>
                     </div>
 
-                    <Button type="submit" className="mt-4 w-full" tabIndex={4} disabled={processing}>
+                    <Button type="submit" color="primary" className="mt-4 w-full" tabIndex={4} disabled={processing}>
                         {processing && <Spinner size="sm" variant="gradient" />}
                         Log in
                     </Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,33 @@ import { heroui } from '@heroui/react';
 export default {
     content: ['./resources/**/*.{js,jsx,ts,tsx,blade.php}', './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                nunito: ['Nunito Sans', 'sans-serif'],
+            },
+            colors: {
+                'smoke-white': '#F5F5F5',
+                primary: '#7286D3',
+                accent: '#8EA7E9',
+            },
+        },
     },
     darkMode: 'class',
     plugins: [
         require('tailwindcss-animate'),
         heroui({
             themes: {
-                colors: {
-                    primary: '#7286D3',
-                    accent: '#8EA7E9',
+                light: {
+                    primary: {
+                        DEFAULT: '#7286D3',
+                        foreground: '#8EA7E9',
+                    },
+                },
+                dark: {
+                    primary: {
+                        DEFAULT: '#7286D3',
+                        foreground: '#8EA7E9',
+                    },
                 },
             },
         }),
